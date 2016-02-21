0e4cb57060772287258dc52518ebee770d351c7c
Navneet Pandey <npandey057@gmail.com>
2016-02-22 00:01:41 +0530

    RainDrops Complete

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,14 @@
+class Raindrops
+	
+	VERSION = 1
+	RAINDROPS = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
+	def self.convert(number)
+		str = number.to_s
+		
+		RAINDROPS.each do |factor, word|
+			str << word if number % factor == 0
+		end
+		str
+	end
+
+end
