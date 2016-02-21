f5d796714cc71e09a28663ab5c3ba505bd8ab802
Navneet Pandey <npandey057@gmail.com>
2016-02-22 00:30:50 +0530

    Reversed GC test pass RNA-Transcription

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -2,7 +2,7 @@ class Complement
 	VERSION = 3
 
   def self.of_dna(nucleotide)
-    nucleotide.gsub('C', 'G').gsub('G', 'C').gsub('T', 'A').gsub('A', 'U')
+    nucleotide.gsub('G', 'C').gsub('C', 'G').gsub('T', 'A').gsub('A', 'U')
   end
 
 end
