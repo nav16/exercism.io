76d30a70cda9dd6d0d28afe1740749c835005c41
Navneet Pandey <npandey057@gmail.com>
2016-02-21 23:37:03 +0530

    RNA-Transcription Complete

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,18 @@
+class Complement
+	VERSION = 3
+  def self.of_dna(nucleotide)
+    strand = nucleotide.chars
+    arr = transcribe(strand)
+  end
+
+  def self.transcribe(strand)
+    arr = []
+    strand.each  do |letter|
+      arr << 'G' if letter == 'C'
+      arr << 'C' if letter == 'G'
+      arr << 'A' if letter == 'T'
+      arr << 'U' if letter == 'A'
+    end
+    arr.join
+  end
+end
