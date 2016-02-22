2fbf7f97e3b06b3d8e0bdac4de72037e9984e318
Navneet Pandey <npandey057@gmail.com>
2016-02-22 11:02:44 +0530

    Update for Arg Error

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,6 +1,8 @@
 class Hamming
 	def self.compute(a, b)
-		len = [a.length, b.length].min
+		#len = [a.length, b.length].min
+		raise ArgumentError unless a.length == b.length
+		len = a.length
 		count = 0
 		(0..len-1).each do |i|
 			count += 1 if a[i] != b[i]
