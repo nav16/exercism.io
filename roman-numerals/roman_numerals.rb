84dd3caacaa5ac8f80270ac9f71cb351fe4cdd12
Navneet Pandey <navneetpandey@Apples-MacBook-Pro.local>
2016-02-22 13:50:56 +0530

    Roman Numerals Done

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,8 +1,31 @@
-class Roman
-
-	ROMAN = { 1 => "I", 5 => "V", 10 => "X", 50=> "L", 100 => "C", 500 => "D", 1000=> "M" }
-	def self.to_roman(num)
-			num % 
-	end
+class Fixnum
+	VERSION = 1
+	ROMAN_NUM = {
+		1000 => "M",  
+	     900 => "CM",  
+	     500 => "D",  
+	     400 => "CD",
+	     100 => "C",  
+	      90 => "XC",  
+	      50 => "L",  
+	      40 => "XL",  
+	      10 => "X",  
+	        9 => "IX",  
+	        5 => "V",  
+	        4 => "IV",  
+	        1 => "I"
+	    }
+	#REV_HASH = Hash[ROMAN.to_a.reverse]
+		
+	def to_roman
+		num = self
+		str = ''
+		ROMAN_NUM.each do |arab, rome|
+			div = num/arab
+			str.concat(rome * div)
+			num = num % arab
+		end
+		str
+	end	
 
 end
