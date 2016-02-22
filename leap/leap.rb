ae80a214986c78004003efa168ff24846b2767f3
Navneet Pandey <npandey057@gmail.com>
2016-02-22 22:41:15 +0530

    Leap Done -ve test failing

---
+++ leap/leap.rb
@@ -0,0 +1,14 @@
+class Year
+	VERSION = 1
+	def self.leap?(year)
+
+		if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0)) 
+			"Yes, #{year} is a leap year"
+		else
+			"No, #{year} is not a leap year"
+		end
+
+	end
+end
+
+puts Year.leap?(1998)
