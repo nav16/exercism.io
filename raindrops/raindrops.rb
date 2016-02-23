63dc3159429c89f7978f3e27bfb0c627c0212915
Navneet Pandey <npandey057@gmail.com>
2016-02-23 18:01:41 +0530

    Raindrops Indentation Fix

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,14 +1,13 @@
 class Raindrops
-	
-	VERSION = 1
-	RAINDROPS = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
-	def self.convert(number)
-		str = number.to_s
-		
-		RAINDROPS.each do |factor, word|
-			str << word if number % factor == 0
-		end
-		str
-	end
+  VERSION = 1
+  RAINDROPS = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
+
+  def self.convert(number)
+    str = number.to_s
+    RAINDROPS.each do |factor, word|
+      str << word if number % factor == 0
+    end
+    str
+  end
 
 end
