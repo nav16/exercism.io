3c37006d141a825d8d3f1699cbf662463c6425cb
Navneet Pandey <npandey057@gmail.com>
2016-02-23 17:35:25 +0530

    Use Sieve Algo

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,22 +1,25 @@
 class Sieve
+
   def initialize(number)
     @number = number
   end
 
   def primes
-    primes_arr = []
-    (2..@number).each do |num|
-      primes_arr << num if Sieve.prime? num
-    end
-    primes_arr
-  end
-
-  def self.prime?(num)
-    (2..(num/2).floor).each do |i|
-      if num % i == 0
-        return false
+    arr = Array.new(@number) { true  }
+    arr[0] = arr[1] = false
+    (2..(arr.length)).each do |num|
+      sum = num
+      until sum > @number
+        sum = sum + num
+        arr[sum] = false
       end
     end
-    true
+    final_arr = []
+    arr.each_index do |num|
+      final_arr << num if(arr[num] == true)
+    end
+    final_arr
   end
+
 end
+#puts Sieve.new(10).primes
