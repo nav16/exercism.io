ee80084477eb7389c4af982de78a3247c434f896
Navneet Pandey <npandey057@gmail.com>
2016-02-23 17:55:23 +0530

    RNA-Transcription refractor

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,21 +1,22 @@
 class Complement
-	VERSION = 3
+  VERSION = 3
+  STRAND_MATCHS = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U'}
 
   def self.of_dna(nucleotide)
      strand = nucleotide.chars
      arr = transcribe(strand)
    end
 
-   def self.transcribe(strand)
-     arr = []
-     strand.each  do |letter|
-       arr << 'G' if letter == 'C'
-       arr << 'C' if letter == 'G'
-       arr << 'A' if letter == 'T'
-       arr << 'U' if letter == 'A'
-       str = "CGTA"
-		raise ArgumentError if !str.include? letter
-     end
-     arr.join
-   end
+  def self.transcribe(strand)
+    arr = []
+    strand.each  do |letter|
+      STRAND_MATCHS.each do |value, replace|
+        arr << replace if letter == value
+      end
+        str = "CGTA"
+        raise ArgumentError if !str.include? letter
+    end
+    arr.join
+  end
+
 end
