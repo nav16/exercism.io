5ccbda4d69951d4dafb1b6c28029cd3c3ecd3538
Navneet Pandey <npandey057@gmail.com>
2016-02-25 17:11:30 +0530

    Space Age Refractor

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,7 +1,7 @@
 class SpaceAge
 
   def initialize(input_age)
-    @input_age = input_age
+    @input_age = input_age.to_f
   end
 
   def seconds
@@ -9,40 +9,40 @@ class SpaceAge
   end
 
   def on_earth
-    age_on_earth.round(2)
+    age_on_earth
   end
 
   def on_mercury
-    mercury_years = (age_on_earth / 0.2408467).round(2)
+    age_on_earth / 0.2408467
   end
 
   def on_venus
-    venus_years = (age_on_earth / 0.61519726).round(2)
+    age_on_earth / 0.61519726
   end
 
   def on_mars
-    mars_years = (age_on_earth / 1.8808158).round(2)
+    age_on_earth / 1.8808158
   end
 
   def on_jupiter
-    jupiter_years = (age_on_earth / 11.862615).round(2)
+    age_on_earth / 11.862615
   end
 
   def on_saturn
-    saturn_years = (age_on_earth / 29.447498).round(2)
+    age_on_earth / 29.447498
   end
 
   def on_uranus
-    uranus_years = (age_on_earth / 84.016846).round(2)
+    age_on_earth / 84.016846
   end
 
   def on_neptune
-    neptune_years = (age_on_earth / 164.79132).round(2)
+    age_on_earth / 164.79132
   end
 
   private
 
   def age_on_earth
-    (@input_age.to_f/31557600.to_f)
+    @input_age / 31557600
   end
 end
