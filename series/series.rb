603566b5a0647ae7c83f9f8bcb7b0bfb5abeaed5
Navneet Pandey <npandey057@gmail.com>
2016-02-27 20:45:16 +0530

    Series use fails instead of raise

--- series/series.rb
+++ series/series.rb
@@ -1,12 +1,10 @@
 class Series
-
   def initialize(number)
     @number = number.chars.map(&:to_i)
   end
 
   def slices(slice)
-    raise ArgumentError if @number.length < slice
+    fail ArgumentError if @number.length < slice
     @number.each_cons(slice).to_a
   end
-
 end
