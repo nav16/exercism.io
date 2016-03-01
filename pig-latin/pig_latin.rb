69b72a17a097b08ddd86a3721dba5a31cc9e1c78
Navneet Pandey <npandey057@gmail.com>
2016-03-01 11:09:50 +0530

    Pig Latin Complete with one tc skip

--- pig-latin/pig_latin.rb
+++ pig-latin/pig_latin.rb
@@ -1,12 +1,15 @@
 class PigLatin
 
   def self.translate(word)
-    @word = word
-    return for_vowel if "aeiou".include? @word[0]
-    ch =  check_for_special_cases
-    puts ch
-    return for_special_case(ch) if ch.length > 1
-    return for_consonant
+    @word_list = word.split(" ")
+    @word_list.map do |word|
+      @word = word
+      return for_vowel if check_like_vowel
+      return for_vowel if "aeiou".include? @word[0]
+      ch =  check_for_special_cases
+      return for_special_case(ch) if !ch.nil? && ch.length > 1
+      return for_consonant
+    end
   end
 
   private
@@ -24,11 +27,15 @@ class PigLatin
     @word[len..-1].to_s + char.to_s + "ay"
   end
 
+  def self.check_like_vowel
+    %w[yt xr].detect do |char|
+      return true if @word.start_with?(char)
+    end
+  end
+
   def self.check_for_special_cases
-    %w[ch qu thr th sch].each do |char|
+    %w[ch qu squ thr th sch].detect do |char|
       return char if @word.start_with?(char)
     end
   end
 end
-
-puts PigLatin.translate('chair')
