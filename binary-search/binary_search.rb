07b8e3d3cac71007ebfed2a954848fe7a3df693a
Navneet Pandey <npandey057@gmail.com>
2016-03-01 23:19:30 +0530

    Binary Search Implemented

--- binary-search/binary_search.rb
+++ binary-search/binary_search.rb
@@ -1,23 +1,37 @@
 class BinarySearch
+  attr_reader :list
 
-  def initialize(list)
-    @list = list
-    fail ArgumentError if @list != @list.sort
-  end
-
-  def list
-    @list
-  end
-
-  def search_for(number)
-    output = @list.index number
-    fail RuntimeError if output.nil?
-    output
+  def initialize(input)
+    fail ArgumentError unless input.each_cons(2).all? { |a, b| b > a }
+    @list = input
   end
 
   def middle
-    len = @list.length
-    @list[len % 2]
+    find_middle(low = 0, high = @list.size - 1)
+  end
+
+  def search_for(number)
+    call_recursive(number, low = 0, high = @list.size - 1)
+  end
+
+  private
+
+  def find_middle(low, high)
+    fail RuntimeError if @list[low..high].empty?
+    (low + high) / 2
+  end
+
+  def call_recursive(number, low, high)
+    mid = find_middle(low, high)
+    case
+    when @list[mid] == number
+      mid
+    when @list[mid] > number
+      high = mid - 1
+      call_recursive(number, low, high)
+    else
+      low = mid + 1
+      call_recursive(number, low, high)
+    end
   end
 end
-
