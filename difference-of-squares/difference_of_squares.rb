d13baef9b93e544275ab5138977e06b67f06ce08
Navneet Pandey <npandey057@gmail.com>
2016-03-18 12:21:58 +0530

    use inject

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,26 +1,18 @@
 class Squares
-	VERSION = 2
-	def initialize(number)
-		@number = number
-	end
+ VERSION = 2
+ def initialize(number)
+  @number = number
+ end
 
-	def square_of_sum
-		sum = 0
-		(1..@number).each do |i|
-			sum += i
-		end
-		sum ** 2
-	end
+ def square_of_sum
+  (1..@number).inject(0) { |sum, i| sum + i } ** 2
+ end
 
-	def sum_of_squares
-		sum = 0
-		(1..@number).each do |i|
-			sum += i ** 2
-		end
-		sum
-	end
+ def sum_of_squares
+  (1..@number).inject(0) { |sum, i| sum + (i ** 2) }
+ end
 
-	def difference
-		square_of_sum - sum_of_squares
-	end
+ def difference
+  square_of_sum - sum_of_squares
+ end
 end
