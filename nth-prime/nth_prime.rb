7eee8164eb95c8c82570d3679d776f98a10aef3a
Navneet Pandey <npandey057@gmail.com>
2016-03-21 15:14:01 +0530

    nth-prime code refrator

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,31 +1,24 @@
 class Prime
+  def self.nth(num)
+    fail ArgumentError if num == 0
+    start, prime_no, flag = 2, '', false
+    loop do
+      flag = Prime.prime?(start)
+      if flag
+        prime_no = start
+        num -= 1
+      end
+      start = start.next
+      break if num < 1
+    end
+    prime_no.to_i
+  end
 
-	def self.nth(num)
-		raise ArgumentError if num == 0
-		start = 2
-		prime_no = ''
-		flag = false
-		loop do 
-			
-			flag = Prime.prime?(start)
-			if flag
-				prime_no = start
-				num -= 1
-			end
-			start = start.next
-		 	break if num < 1 
-		end
-		prime_no.to_i
-	end
-
-	def self.prime?(num)
-	    (2..(num/2).floor).each do |i|
-	      if num % i == 0
-	        return false
-	      end
-	    end
-	    true
-	end
-		
+  def self.prime?(num)
+    (2..(num/2).floor).each do |i|
+      return false if num % i == 0
+    end
+    true
+  end
 end
 
